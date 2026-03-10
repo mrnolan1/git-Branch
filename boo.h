1a40aba1b33cb409bc3d0bdaaf5d81c0f1c7767f
Matthew <lrnolan@nic.edu>
2026-03-10 14:22:07 -0700

    boo is done

---
+++ boo.h
@@ -0,0 +1,6 @@
+#include <iostream>
+
+void boo()
+{
+    std::cout << "hello from boo\n";
+}
--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include "foo.h"
+#include "boo.h"
 
 int main()
 {
     std::cout << "Hello World\n";
     foo();
+    boo();
     for(int i = 0; i < 10; i++)
     {
         for(int j = 0; j < 10; j++)
